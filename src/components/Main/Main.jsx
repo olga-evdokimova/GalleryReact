369a677af4d6b4ff8bc6fa47c387020e9d517db5
olga-evdokimova <pavedom13@gmail.com>
2022-11-30 12:48:51 +0300

    build

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,47 +1,41 @@
 import m from "./Main.scss";
 import Slide from "./Slide/Slide";
-import { useRef, useState } from 'react'; 
+import { useRef, useState } from "react";
 
 const Main = () => {
-    const divRef = useRef()
-    const [isDown, setDown] = useState(false)
-    return (
+  const divRef = useRef();
+    const [isDown, setDown] = useState(false);
+    
+  
 
-        <main className="page">
-            <div className="gallery">
-                <div className="gallery__wrapper"
-                    ref={divRef}
-                    onWheel={(item) => {
-                        const div = divRef.current
-                        const wheel = item.deltaY
-                        // console.log(item.deltaY)
-                        const newScroll = div.scrollLeft + wheel / 5
-                        div.scrollLeft = newScroll
-                    }}
-
-                    onMouseDown={(item) => {
-                        console.log("down!")
-                        // setDown(true) // down = true
-                    }}
-
-                    onMouseMove={(item) => {
-                        console.log("move!")
-                    }}
-                >
-                    <Slide name="Название" url="https://i.ibb.co/2hWjYx1/image-01.jpg"/>
-                    <Slide name="Какое-то фото" url="https://i.ibb.co/wznpmzy/image-02.jpg"/>
-                    <Slide name="Еще название" url="https://i.ibb.co/kHXr21y/image-03.jpg"/>
-                    <Slide name="Название" url=" https://i.ibb.co/zFPFs16/image-04.jpg"/>
-                    <Slide name="Какое-то фото" url="https://i.ibb.co/CBSMK27/image-05.jpg"/>
-                    <Slide name="Еще название" url="https://i.ibb.co/JxYjps3/image-06.jpg"/>
-                    <Slide name="Еще название" url="https://i.ibb.co/8BrghxK/image-07.jpg"/>
-                    <Slide name="Название" url="https://i.ibb.co/0fQKwz3/image-08.jpg"/>
-                </div>
-            </div>
-
-        </main>
-
-    );
-}
+  return (
+    <main className="page">
+      <div className="gallery">
+        <div
+          className="gallery__wrapper"
+          ref={divRef}
+          onWheel={(item) => {
+            const div = divRef.current;
+            const wheel = item.deltaY;
+            // console.log(item.deltaY)
+            const newScroll = div.scrollLeft + wheel / 5;
+            div.scrollLeft = newScroll;
+          }}
+          onMouseDown={(item) => {
+            console.log("down!");
+            // setDown(true) // down = true
+          }}
+          onMouseMove={(item) => {
+            console.log("move!");
+          }}
+        >
+          
+          <Slide />
+          <Slide />
+          <Slide />
+        </div>
+      </div>
+    </main>
+  );
+};
 export default Main;
-
--- src/components/Main/Slide/Slide.jsx
+++ src/components/Main/Slide/Slide.jsx
@@ -1,12 +1,15 @@
 import m from "./Slide.scss";
 
 const Slide = (props) => {
+  const slides = ["Первое", "Второе", "Третье"];
+  const listItems = slides.map((slide) => <li>{slide}</li>);
+  
   return (
     <div className="slide">
       <div className="slide__image">
-        <img src={props.url} alt={props.name}  />
+        <img src={props.url} alt={props.name} />
       </div>
-      <span>{props.name}</span>
+      <span>{listItems}</span>
     </div>
   );
 };
